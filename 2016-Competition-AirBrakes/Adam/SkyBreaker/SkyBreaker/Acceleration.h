0db09bb4c94465e8c322a5d3bf353b9ac04eaf40
Th3n3wd4wn <TH3N3WD4WN@users.noreply.github.com>
2016-03-05 14:00:43 -0600

    Added Sensor test project to my folder

---
+++ 2016-Competition-AirBrakes/Adam/SkyBreaker/SkyBreaker/Acceleration.h
@@ -0,0 +1,22 @@
+#ifndef _ACCELERATION_H
+#define _ACCELERATION_H
+#define ARDUINO 9
+
+#include "Sensor.h"
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BNO055.h>
+#include <utility/imumaths.h>
+
+class acceleration: public Sensor
+{
+public:
+private:
+   
+   Adafruit_BNO055 bno = Adafruit_BNO055();
+
+   float xAccel;
+   float yAccel;
+   float zAccel;
+}
+
+#endif
---
+++ 2016-Competition-AirBrakes/Adam/SkyBreaker/SkyBreaker/Sensor.h
@@ -0,0 +1,14 @@
+#ifndef _SENSOR_H
+#define _SENSOR_H
+
+class Sensor
+{
+public:
+   void update();
+   void initialize();
+   void getID();
+private:
+
+};
+
+#endif
