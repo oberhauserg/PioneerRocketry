5778faa1a27a32d8d8d3212c5bcadae54697b0f2
jacobkn <jacobkn@yahoo.com>
2016-04-04 09:07:59 -0500

    Created base sensor class.

---
+++ 2016-Competition-AirBrakes/TestProjects/Sensor/Sensor.h
@@ -0,0 +1,15 @@
+#ifndef _SENSOR_H
+#define _SENSOR_H
+
+class Sensor
+{
+public:
+	bool Initialize();
+	void Update();
+	String GetID() { return id; }
+	void SetID(String _id) {id = _id}
+private:
+	String id;
+};
+
+#endif _SENSOR_H
---
+++ 2016-Competition-AirBrakes/TestProjects/Sensor/Sensor.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		Sensor.ino
+ Created:	4/4/2016 8:42:01
+ Author:	Jacob
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
