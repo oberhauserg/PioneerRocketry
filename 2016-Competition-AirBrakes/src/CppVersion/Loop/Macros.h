07f0014d8e07eeb8c3e50d29009291864a9a5af0
Th3n3wd4wn <TH3N3WD4WN@users.noreply.github.com>
2016-04-21 17:19:01 -0500

    Forgot to add Macros.h

---
+++ 2016-Competition-AirBrakes/src/CppVersion/Loop/Macros.h
@@ -0,0 +1,19 @@
+#ifndef _MACROS_H
+#define _MACROS_H
+//-------------------------------------------------
+// This header contains all the macros for testing
+// and utilities
+//-------------------------------------------------
+
+// Debugging Macros
+#define TESTING_EVENT			// Event.h
+#define TESTING_EVENT_			// Event.h
+#define DEBUG_KALMAN			// Kalman.h
+#define DEBUG_SENSORS			// SensorHub.h
+#define USING_SERIAL_MONITOR	        // XBee.h
+
+// Utility Macros
+#define NO_PITOT		        // SensorHub.h
+#define _DONT_WAIT_FOR_STRATO	        // Stratologger.h
+
+#endif
